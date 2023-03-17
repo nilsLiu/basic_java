08fcb5bccd9e57c3d5cfa93e96897a1c279e96ab
liuch <724058434@qq.com>
2023-03-17 12:27:23 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/com/lch/Annotation/AnnotationTest.java
@@ -0,0 +1,10 @@
+package com.lch.Annotation;
+
+public class AnnotationTest {
+
+    @MyAnnotation("hi")
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/lch/Annotation/MyAnnotation.java
@@ -0,0 +1,17 @@
+package com.lch.Annotation;
+
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.*;
+
+
+/**
+ * @author liuch
+ */
+
+
+
+public @interface MyAnnotation {
+
+    String value();
+}
